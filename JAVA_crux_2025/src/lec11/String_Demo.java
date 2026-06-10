c0ef4a9f35623afeed473218177836a510a39ba3
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-06-11 01:11:45 +0530

    lec 10 String started

---
+++ JAVA_crux_2025/src/lec11/String_Demo.java
@@ -0,0 +1,13 @@
+package lec11;
+
+public class String_Demo { // String is a class in JAVA 
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+String str="hello";
+String str1=new String("hello");
+String str2="hello";
+String str3=new String("hello");
+	}
+
+}
