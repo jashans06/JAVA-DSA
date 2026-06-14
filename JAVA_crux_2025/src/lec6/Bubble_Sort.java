b9a66d0d5dc95bb6af5edd5abfe3cca68df41d75
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-06-14 22:47:24 +0530

    lec 6 bubble sort revised

--- JAVA_crux_2025/src/lec6/Bubble_Sort.java
+++ JAVA_crux_2025/src/lec6/Bubble_Sort.java
@@ -9,12 +9,14 @@ public class Bubble_Sort {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
-		
+
 	}
 
 	public static void Sort(int[] arr) {
 		for (int turn = 1; turn < arr.length; turn++) {
-			for (int i = 0; i < arr.length - turn; i++) {
+			for (int i = 0; i < arr.length - turn; i++) {// arr.length - turn ta kita kyuki 1 turn to baad sab to vadda
+				// element last position te a chuka hunda te turn 2 vich 2
+				// largest element 2nd last position ch aa chuka hunda
 				if (arr[i] > arr[i + 1]) {
 					int temp = arr[i];
 					arr[i] = arr[i + 1];
