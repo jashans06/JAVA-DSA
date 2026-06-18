0fb102e0c00cfe2fd320a6a343ceeee07d678856
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-06-19 01:05:11 +0530

    Recursion Continued..2

---
+++ JAVA_crux_2025/src/lec16/SubSequence.java
@@ -0,0 +1,22 @@
+package lec16;
+
+public class SubSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ques = "abc";
+		PrintSubSequence(ques, "");
+	}
+
+	public static void PrintSubSequence(String ques, String ans) {
+		// TODO Auto-generated method stub
+		if (ques.length() == 0) {
+			System.out.print(ans + " ");
+			return;
+		}
+		char ch = ques.charAt(0);
+		PrintSubSequence(ques.substring(1), ans);
+		PrintSubSequence(ques.substring(1), ans + ch);
+	}
+
+}
