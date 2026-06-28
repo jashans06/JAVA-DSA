8083fb38366182d2e59d75585d52afb3434e8673
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-06-28 23:59:12 +0530

    function name changes in coin combination ques

--- JAVA_crux_2025/src/lec21/Coin_Combination.java
+++ JAVA_crux_2025/src/lec21/Coin_Combination.java
@@ -5,17 +5,17 @@ public class Coin_Combination {
 	public static void main(String[] args) {
 		int[] coin = { 2, 1, 3, 5 };
 		int amount = 4;
-		permutation(coin, amount, "", 0);
+		combination(coin, amount, "", 0);
 	}
 
-	public static void permutation(int[] coin, int amount, String ans, int index) {
+	public static void combination(int[] coin, int amount, String ans, int index) {
 		if (amount == 0) {
 			System.out.println(ans);
 			return;
 		}
 		for (int i = index; i < coin.length; i++) {
 			if (amount >= coin[i]) {
-				permutation(coin, amount - coin[i], ans + coin[i], i);
+				combination(coin, amount - coin[i], ans + coin[i], i);
 			}
 		}
 	}
