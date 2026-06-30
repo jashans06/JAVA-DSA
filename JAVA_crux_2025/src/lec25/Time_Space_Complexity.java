b8e32870e54156535c073009ba785e7c87071966
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-06-30 18:07:49 +0530

    Time and Space complexity continued

--- JAVA_crux_2025/src/lec25/Time_Space_Complexity.java
+++ JAVA_crux_2025/src/lec25/Time_Space_Complexity.java
@@ -10,122 +10,110 @@ public class Time_Space_Complexity {
 		}
 		long end = System.currentTimeMillis();
 		System.out.println(end - start);
-	
 
-	int n = 176781671;
-	int i = 0;
-	
-	while(i<n)
-	{
-		System.out.println("Hey");
-		// O(N)
-		i++;
-	}
-	while(i<=n)
-	{
-		System.out.println("Hey");
-		// log(N) base 2
-		i *= 2;
-	}
-	while(n>0)
-	{
-		System.out.println("Hey");
-		n /= 2;
-	}
-	while(i<=n)
-	{
-		System.out.println("Hey");
-		// O(N)
-		i += 2;
-		i += 3;
-	}
-	while(i<=n)
-	{
-		System.out.println("Hey");
-		// log(N) base 6
-		i *= 2;
-		i *= 3;
-	}
-	int k = 3;while(n>0)
-	{
-		System.out.println("Hey");
-		// log(N) base 6
-		n /= 2;
-		n /= 3;
-	}
+		int n = 176781671;
+		int i = 0;
 
-	while(i<=n)
-	{
-		System.out.println("Hey");
-		// O(N/K)
-		i += k;
-	}
-
-	while(i<=n)
-	{
-		System.out.println("Hey");
-		// log(N) base k
-		i *= k;
-	}while(n>0)
-	{
-		System.out.println("Hey");
-		// O(N)
-		n = n - 1;
-	}while(n>0)
-	{
-		System.out.println("Hey");
-		// O(N)
-		n = n - 2;
-		n = n - 3;
-	}while(n>0)
-	{
-		n = n - k;
-		// o(N/k)
-	}for(i=1;i<=n;i++)
-	{
-		for (int j = 1; j <= n; j++) {
-			// O(N^2)
-			System.out.println("hey");
+		while (i < n) {
+			System.out.println("Hey");
+			// O(N)
+			i++;
 		}
-	}
-
-	for(i=1;i*i<=n;i++)
-	{
-		System.out.println("hey");
-		// sqrt(N)
-	}
-
-	for(i=1;i<=n;i++)
-	{
-		for (int j = 1; j <= i; j++) {
-			for (k = 1; k <= n; k++) {
-				// O(N^3)
-			}
+		while (i <= n) {
+			System.out.println("Hey");
+			// log(N) base 2
+			i *= 2;
 		}
-	}
+		while (n > 0) {
+			System.out.println("Hey");
+			n /= 2;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(N)
+			i += 2;
+			i += 3;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// log(N) base 6
+			i *= 2;
+			i *= 3;
+		}
+		int k = 3;
+		while (n > 0) {
+			System.out.println("Hey");
+			// log(N) base 6
+			n /= 2;
+			n /= 3;
+		}
 
-	for(i=1;i<=n;i++)
-	{
-		for (int j = 1; j <= i * i; j++) {
-			for (k = 1; k <= n / 2; k++) {
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(N/K)
+			i += k;
+		}
+
+		while (i <= n) {
+			System.out.println("Hey");
+			// log(N) base k
+			i *= k;
+		}
+		while (n > 0) {
+			System.out.println("Hey");
+			// O(N)
+			n = n - 1;
+		}
+		while (n > 0) {
+			System.out.println("Hey");
+			// O(N)
+			n = n - 2;
+			n = n - 3;
+		}
+		while (n > 0) {
+			n = n - k;
+			// o(N/k)
+		}
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				// O(N^2)
 				System.out.println("hey");
-
-				// O(N^4)
 			}
 		}
-	}
 
-	for(i=1;i<=n;i++)
-	{
-		for (int j = 1; j <= n; j += i) {
+		for (i = 1; i * i <= n; i++) {
 			System.out.println("hey");
+			// sqrt(N)
 		}
+
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				for (k = 1; k <= n; k++) {
+					// O(N^3)
+				}
+			}
+		}
+
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= i * i; j++) {
+				for (k = 1; k <= n / 2; k++) {
+					System.out.println("hey");
+
+					// O(N^4)
+				}
+			}
+		}
+
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j += i) {
+				System.out.println("hey");
+			}
+		}
+
+		// bubble
+		// Selection
+		// instersion
+
 	}
-
-	// bubble
-	// Selection
-	// instersion
-
-}
 
 }
