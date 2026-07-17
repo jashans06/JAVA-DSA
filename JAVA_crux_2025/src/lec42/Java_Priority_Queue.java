17a5f3d64f7e8e942e0c60c5780f2435f67bd388
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-07-17 18:25:50 +0530

    HEAP started

---
+++ JAVA_crux_2025/src/lec42/Java_Priority_Queue.java
@@ -0,0 +1,15 @@
+package lec42;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Java_Priority_Queue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PriorityQueue<Integer> pq = new PriorityQueue<>();// min heap
+		PriorityQueue<Integer> pq1 = new PriorityQueue<>(Collections.reverseOrder());// max heap
+
+	}
+
+}
