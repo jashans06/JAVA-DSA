8750a25dde97defc13a91a2a7008572663ec3b8a
Jashanpreet Singh <jashanpreets0703@gmail.com>
2026-08-05 23:50:10 +0530

    Bitmasking continued

---
+++ JAVA_crux_2025/src/lec54/Single_Number_1.java
@@ -0,0 +1,20 @@
+package lec54;
+
+public class Single_Number_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 4, 1, 2, 1, 2 };
+System.out.println(Single_Number(arr));
+	}
+
+	public static int Single_Number(int[] arr) {
+
+		int ans = 0;
+		for (int i = 0; i < arr.length; i++) {
+			ans ^= arr[i];// ans = ans ^arr[i];
+		}
+		return ans;
+	}
+
+}
